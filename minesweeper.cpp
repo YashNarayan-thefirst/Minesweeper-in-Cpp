b7f13c15ca78b13146f24d3637c8c26c939e3687
YashNarayan-thefirst <106808693+YashNarayan-thefirst@users.noreply.github.com>
2024-12-04 19:48:43 -0500

    fixed hundred's digit rendering

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -20,42 +20,61 @@ void restart() {
 void render() {
     auto& t = ToolBox::getInstance();
     const int flags = t.gameState->getMineCount() - t.gameState->getFlagCount();
-    bool neg = std::abs(t.gameState->getMineCount());
+    const bool neg = (flags < 0);
 
-    // Calculate digits
-    int digs[] = {flags % 10, (flags / 10) % 10,0};
+    int absFlags = std::abs(flags);
 
-    // Map digit sprites
+    int digs[] = {
+        absFlags / 100 % 10, // Hundreds place
+        absFlags / 10 % 10,  // Tens place
+        absFlags % 10        // Units place
+    };
+
     sf::IntRect rects[] = {
         {0, 0, 21, 32}, {21, 0, 21, 32}, {42, 0, 21, 32}, {63, 0, 21, 32},
         {84, 0, 21, 32}, {105, 0, 21, 32}, {126, 0, 21, 32}, {147, 0, 21, 32},
-        {168, 0, 21, 32}, {189, 0, 21, 32}, {210, 0, 21, 32}  // Last is negative
+        {168, 0, 21, 32}, {189, 0, 21, 32}, {210, 0, 21, 32} // Last is negative
     };
 
-    sf::Sprite sps[] = {sf::Sprite(t.digits), sf::Sprite(t.digits), sf::Sprite(t.digits)};
-    for (int i = 0; i < 3; i++) sps[i].setTextureRect(rects[i == 2 && neg ? 10 : digs[i]]);
-    sps[0].setPosition(42, 512), sps[1].setPosition(21, 512), sps[2].setPosition(0, 512);
-    for (auto& s : sps) t.window.draw(s);
-
-    // Set button sprites
-    sf::Sprite* btnSprites[] = {
-        new sf::Sprite(t.debug), new sf::Sprite(t.test1), new sf::Sprite(t.test2)
+    // Create sprites for the digits
+    sf::Sprite sps[] = {
+        sf::Sprite(t.digits),
+        sf::Sprite(t.digits),
+        sf::Sprite(t.digits)
     };
+
+    for (int i = 0; i < 3; i++) {
+        sps[i].setTextureRect(rects[(i == 0 && neg) ? 10 : digs[i]]);
+    }
+
+    // Position the sprites
+    sps[0].setPosition(0, 512);
+    sps[1].setPosition(21, 512);
+    sps[2].setPosition(42, 512);
+
+    for (auto& s : sps)t.window.draw(s);
+
+    sf::Sprite* btnSprites[] = {new sf::Sprite(t.debug), new sf::Sprite(t.test1), new sf::Sprite(t.test2)};
     t.debugButton->setSprite(btnSprites[0]);
     t.testButton1->setSprite(btnSprites[1]);
     t.testButton2->setSprite(btnSprites[2]);
 
     // Set new game button based on state
     t.newGameButton->setSprite(new sf::Sprite(
-        t.gameState->getPlayStatus() == GameState::PLAYING? t.happy: (t.gameState->getPlayStatus() == GameState::WIN ? t.win : t.lose)));
+        t.gameState->getPlayStatus() == GameState::PLAYING ? t.happy :
+        (t.gameState->getPlayStatus() == GameState::WIN ? t.win : t.lose)));
 
     // Set button positions
     Button* btns[] = {t.debugButton, t.newGameButton, t.testButton1, t.testButton2};
-    for (auto* b : btns)
-        b->getSprite()->setPosition(b->getPosition().x, b->getPosition().y);
+    for (auto* b : btns) b->getSprite()->setPosition(b->getPosition().x, b->getPosition().y);
     for (auto* b : btns) t.window.draw(*b->getSprite());
-    for (int j = 0; j < t.board.size(); j++)
-        for (int k = 0; k < t.board[0].size(); k++) t.gameState->getTile(k, j)->draw();
+    
+    // Draw the game board
+    for (int j = 0; j < t.board.size(); j++) {
+        for (int k = 0; k < t.board[0].size(); k++) {
+            t.gameState->getTile(k, j)->draw();
+        }
+    }
 }
 
 int launch() {
